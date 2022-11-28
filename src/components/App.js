c680e8962133dd4103f2cfe34aac2a7d4a4eb7f6
lvaldenebro <lauravaldeal@gmail.com>
2022-11-28 09:37:07 +0100

    Adds react code to print all adalabers' data

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
 // Fichero src/components/App.js
 import '../styles/App.css';
+import {useState} from 'react';
+import adalabers from "../data/adalabers.json";
 
 function App() {
+  const [data, setData] = useState(adalabers.results); //be careful with the given structure, this is an object with an array with an object
+
+  const printData = data.map((eachAdalaber, index) => {
+    return (
+      <tr key={index}>
+        <td>{eachAdalaber.name}</td>
+        <td>{eachAdalaber.counselor}</td>
+        <td>{eachAdalaber.speciality}</td>
+      </tr>
+    );
+  });
+
   return (
     <div>
       <h1>Adalabers</h1>
       <form>
-        <label className="searchNameLabel" for="searchName">Nombre:</label>
+        <label className="searchNameLabel" htmlFor="searchName">Nombre:</label>
         <input
               className="searchName"
               type="search"
               name="searchName"
               placeholder="Ej: MariCarmen"
             />
-        <label className="searchTeacherLabel" for="searchTeacher">Escoge una tutora:</label>
+        <label className="searchTeacherLabel" htmlFor="searchTeacher">Escoge una tutora:</label>
         <select
               className="searchTeacher"
               type="search"
@@ -32,40 +46,24 @@ function App() {
           <th>Especialidad</th>
         </tr></thead>
 
-        <tbody>
-          <tr>
-            <td>MariCarmen</td>
-            <td>Yanelis</td>
-            <td>Python</td>
-          </tr>
-          <tr>
-            <td>Amparo</td>
-            <td>Dayana</td>
-            <td>IA</td>
-          </tr>
-          <tr>
-            <td>Escandia</td>
-            <td>Iván</td>
-            <td>3D graphics</td>
-          </tr>
-        </tbody>
+        <tbody>{printData}</tbody>
       </table>
 
       <form>
-        <fieldset> Añadir una Adalaber</fieldset>
-        <label className="addNameLabel" for="addName">Nombre:</label>
+        <h2>Añadir una Adalaber</h2>
+        <label className="addNameLabel" htmlFor="addName">Nombre:</label>
         <input
               className="addName"
               type="text"
               name="addName"
             />
-        <label className="addTeacherLabel" for="addTeacher">Tutora:</label>
+        <label className="addTeacherLabel" htmlFor="addTeacher">Tutora:</label>
         <input
               className="addTeacher"
               type="text"
               name="addTeacher"
             />
-        <label className="addSpecialityLabel" for="addSpeciality">Especialidad:</label>
+        <label className="addSpecialityLabel" htmlFor="addSpeciality">Especialidad:</label>
         <input
               className="addSpeciality"
               type="text"
